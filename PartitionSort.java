1aa8f4872bb83f7ca6daef135e61b9cc208c449e
Amanda Zheng <azheng00@stuy.edu>
2019-03-09 12:57:09 -0500

    forgot return statement

--- PartitionSort.java
+++ PartitionSort.java
@@ -8,22 +8,25 @@ public class PartitionSort{
     //temporary used during switching
     data[start]=data[x];
     data[x]=temp;
-
+    //switch values to make pivot first one
     for (int i=start+1;i<end+1;i++ ) {
       for (int h=0;h<data.length ;h++ ) {
         System.out.print(data[h]);
       }
       System.out.println(" ");
-      if (data[i]<=y){
+      if (data[i]<data[start]){
         pos++;
         temp=data[pos];
         data[pos]=data[i];
         data[i]=temp;
     }
+    //if the number is less than pivot, increase the position and then switch
   }
   temp=data[start];
   data[start]=data[pos];
   data[pos]=temp;
+  //switch pivot point to the pos
+  return pos;
 }
   public static void main(String[] args) {
     int[] hi={2,3,6,2,4,1};
