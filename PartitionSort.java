577f92403802ab6b1b1f6309381c2539d323f1eb
Amanda Zheng <azheng00@stuy.edu>
2019-03-09 13:13:22 -0500

    took out print statements

--- PartitionSort.java
+++ PartitionSort.java
@@ -3,7 +3,7 @@ public class PartitionSort{
   public static int partition(int[] data,int start, int end){
     int pos=start;
     int x=(int)(Math.random()*100%(end-start+1))+start;
-    System.out.println(x);
+    //System.out.println(x);
     //holds index of chosen pivot point
     int temp=data[start];
     //temporary used during switching
@@ -11,10 +11,10 @@ public class PartitionSort{
     data[x]=temp;
     //switch values to make pivot first one
     for (int i=start+1;i<end+1;i++ ) {
-      for (int h=0;h<data.length ;h++ ) {
+      /*for (int h=0;h<data.length ;h++ ) {
         System.out.print(data[h]+" ");
       }
-      System.out.println(" ");
+      System.out.println(" ");*/
       if (data[i]<data[start]){
         pos++;
         temp=data[pos];
@@ -23,22 +23,22 @@ public class PartitionSort{
     }
     //if the number is less than pivot, increase the position and then switch
   }
-  for (int h=0;h<data.length ;h++ ) {
+  /*for (int h=0;h<data.length ;h++ ) {
     System.out.print(data[h]+" ");
   }
-  System.out.println(" ");
+  System.out.println(" ");*/
   temp=data[start];
   data[start]=data[pos];
   data[pos]=temp;
   //switch pivot point to the pos
-  for (int h=0;h<data.length ;h++ ) {
+  /*for (int h=0;h<data.length ;h++ ) {
     System.out.print(data[h]+" ");
   }
-  System.out.println(" ");
+  System.out.println(" ");*/
   return pos;
 }
-  public static void main(String[] args) {
+  /*public static void main(String[] args) {
     int[] hi={1,43,2,23,8,-4,23};
     System.out.println(partition(hi,0,5));
-  }
+  }*/
 }
