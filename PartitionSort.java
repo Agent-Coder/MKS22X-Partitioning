964743b99562239af33ba2982f6d367469dc8d6a
Amanda Zheng <azheng00@stuy.edu>
2019-03-09 13:11:57 -0500

    random's range was wrong before but its ok now

--- PartitionSort.java
+++ PartitionSort.java
@@ -2,7 +2,8 @@ import java.lang.Math;
 public class PartitionSort{
   public static int partition(int[] data,int start, int end){
     int pos=start;
-    int x=(int)(Math.random()*100%data.length)+start;
+    int x=(int)(Math.random()*100%(end-start+1))+start;
+    System.out.println(x);
     //holds index of chosen pivot point
     int temp=data[start];
     //temporary used during switching
@@ -11,7 +12,7 @@ public class PartitionSort{
     //switch values to make pivot first one
     for (int i=start+1;i<end+1;i++ ) {
       for (int h=0;h<data.length ;h++ ) {
-        System.out.print(data[h]);
+        System.out.print(data[h]+" ");
       }
       System.out.println(" ");
       if (data[i]<data[start]){
@@ -22,14 +23,22 @@ public class PartitionSort{
     }
     //if the number is less than pivot, increase the position and then switch
   }
+  for (int h=0;h<data.length ;h++ ) {
+    System.out.print(data[h]+" ");
+  }
+  System.out.println(" ");
   temp=data[start];
   data[start]=data[pos];
   data[pos]=temp;
   //switch pivot point to the pos
+  for (int h=0;h<data.length ;h++ ) {
+    System.out.print(data[h]+" ");
+  }
+  System.out.println(" ");
   return pos;
 }
   public static void main(String[] args) {
-    int[] hi={2,3,6,2,4,1};
+    int[] hi={1,43,2,23,8,-4,23};
     System.out.println(partition(hi,0,5));
   }
 }
